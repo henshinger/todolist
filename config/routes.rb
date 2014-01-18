acaf184d377f7509a80f5cedf144d01cbfed3936
KenleyNitrous <kenleytan@yahoo.com>
2014-01-18 09:53:52 +0000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Todolist::Application.routes.draw do
   resources :tasks
 
-  get "navigation/home"
-  get "navigation/help"
+  root "navigation#home"
+  get 'help' => "navigation#help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
