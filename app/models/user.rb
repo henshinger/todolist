786cb4ace9142bc6b04ff6efbb057100a329e80f
KenleyNitrous <kenleytan@yahoo.com>
2014-01-20 14:56:20 +0000

    Added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  validates_uniqueness_of :email
+  #validates_uniqueness_of :email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
